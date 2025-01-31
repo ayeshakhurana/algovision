062bc0729896441791348558acfba2329ef4fea0
ayeshakhurana <ayesha@Ayeshas-MacBook-Air.local>
2025-01-31 11:20:52 +0530

    inventory pie chart

--- dashboard.py
+++ dashboard.py
@@ -11,4 +11,11 @@ def load_data():
 df=load_data()
 
 st.title('Zepto Analytics Dashboard')
+st.subheader("Key Metrics")
+st.metric("Total Orders", df['Customer_ID'].nunique())
+st.metric("Total Revenue", f"${df['Price'].sum():,.2f}")
+st.metric("Avg Delivery Time (min)", f"{df['Delivery_Time_mins'].mean():.2f}")
 
+st.subheader("1. Optimizing Inventory and Reducing Stockouts")
+fig=px.pie(df, names="Product_Category", title="Product Category Distribution")
+st.plotly_chart(fig)
