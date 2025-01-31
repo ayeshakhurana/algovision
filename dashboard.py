f7a817d7e19e5c9e9cb3a86acb1b0fbcb8488cf3
ayeshakhurana <ayesha@Ayeshas-MacBook-Air.local>
2025-01-31 22:12:22 +0530

    added charts

--- dashboard.py
+++ dashboard.py
@@ -10,12 +10,54 @@ def load_data():
 
 df=load_data()
 
-st.title('Zepto Analytics Dashboard')
-st.subheader("Key Metrics")
+st.title('Zepto\'s Analytics Dashboard')
+st.markdown("<h3 style='text-decoration: underline;'>Key Metrics</h3>", unsafe_allow_html=True)
 st.metric("Total Orders", df['Customer_ID'].nunique())
 st.metric("Total Revenue", f"${df['Price'].sum():,.2f}")
 st.metric("Avg Delivery Time (min)", f"{df['Delivery_Time_mins'].mean():.2f}")
 
-st.subheader("1. Optimizing Inventory and Reducing Stockouts")
-fig=px.pie(df, names="Product_Category", title="Product Category Distribution")
+st.markdown("<h3 style='text-decoration: underline;'>Insights derived from the Dataset</h3>", unsafe_allow_html=True)
+
+st.header("_1. Optimizing Inventory and Reducing Stockouts_")
+fig=px.bar(df, x='Product_Category', title='Top Selling Products', y='Quantity',color='Product_Category',color_discrete_sequence=px.colors.qualitative.Set1)
 st.plotly_chart(fig)
+
+fig1=px.pie(df, names='Product_Category', title='Product Category Distribution')
+st.plotly_chart(fig1)
+
+
+st.markdown("""
+### Exploratory data analysis (EDA) results
+
+- **As we can see through the pie chart**, the top-selling products include **Dairy, Snacks, and Household Items**.
+- **We can optimize inventory** by increasing stock of these products and reducing stock of less popular products such as **Beverages and Groceries**.**This will help** in reducing stockouts and increasing sales.
+""")
+
+st.header('_2.Enhance delievery time and meet 10 minute promise_')
+
+
+fig=px.bar(df, x='City', title='Average Delivery Time by City', y='Delivery_Time_mins',color='City',color_discrete_sequence=px.colors.qualitative.Set1)
+st.plotly_chart(fig)
+
+fig=px.pie(df, names='Payment_Method', title='City-wise Order Distribution')
+
+st.plotly_chart(fig)
+
+fig=px.histogram(df, x='Gender', title='Delivery Time Distribution')
+st.plotly_chart(fig)
+
+fig = px.pie(df, 
+             names='Payment_Method', 
+             title='Most Opted Payment Method', 
+             color_discrete_sequence=px.colors.qualitative.Set1,
+             hole=0.4)
+st.plotly_chart(fig)
+
+
+
+
+
+
+
+
+
